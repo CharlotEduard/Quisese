1c3ed958487acdbc59af1543cd54486e52aee151
CharlotEduard <50882314+CharlotEduard@users.noreply.github.com>
2019-05-30 13:45:35 -0300

    upgraid Quisese

---
+++ T1/questions.js
@@ -0,0 +1,28 @@
+var questions = [
+
+    ["(Unesp 2014)  Em um plano horizontal encontram-se representadas uma circunferência e as cordas AC e BD. Nas condições apresentadas na figura, determine o valor de x.",
+        "a) x= 5", "b) x= 7", "c) x = 4", "d) x = 3","e) NDA",
+        "img/ex1.png", "", "", "", "","",
+        "2"
+    ],
+    ["(Ufba 2011)  Considere um trapézio T, de altura h = 2u.c., base menor b = 4 u.c. e ângulos da base a = arctg2 e c = 45º.",
+        "a) B= 2", "b) B= 3", "c) B = 7", "d) B = 5","e) NDA",
+        "img/ex2.png", "", "", "", "","",
+        "3"
+    ],
+    ["(Unicamp 2010)  Um artesão precisa recortar um retângulo de couro com   Os dois retalhos de couro disponíveis para a obtenção dessa tira são mostrados nas figuras a seguir.",
+        "a) a. Maior que 3 b. Menor que 2,5", "b) a. Maior que 5 b. Menor que 3", "c) a. Maior que 8 b. Menor que 9,5", "d) a. Maior que 7 b. Menor que 8,5 ","e) NDA",
+        "img/ex3.png", "", "", "", "","",
+        "1"
+    ],
+    ["(Fuvest 2008)  Um poste vertical tem base quadrada de lado   Uma corda de comprimento   está esticada e presa a um ponto   do poste, situado à altura   do solo e distando   da aresta lateral. A extremidade livre   da corda está no solo, conforme indicado na figura. A corda é então enrolada ao longo das faces 1 e 2, mantendo-se esticada e com a extremidade   no solo, até que a corda toque duas arestas da face 2 em pontos   e   conforme a figura.",
+        "a) a. Pr = 3/4 b. AB = 3/5", "b)  a. Pr = 9/5 b. AB = 5/9", "c) a. Pr = 4/3 b. AB = 5/3", "d)  a. Pr = 5/4 b. AB = 5/4 ","e) NDA",
+        "img/ex4.png", "", "", "", "","",
+        "4"
+    ],
+    ["(Ufrrj 2005)  Observe a figura a seguir que demonstra um padrão de harmonia, segundo os gregos.",
+        "a) 1,76 cm", "b) 4,61 m", "c) 1,76 cm", "d) 8,75 m","e) NDA",
+        "img/ex5.png", "", "", "", "","",
+        "3"
+    ],
+]
